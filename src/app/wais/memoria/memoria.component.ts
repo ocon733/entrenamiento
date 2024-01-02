4d03e0cfe942771710e7a8b8216cdd7441353e47
Arturo <arturo@aog.es>
2024-01-02 21:52:30 +0100

    ejercicio de cubos wais

--- src/app/wais/memoria/memoria.component.ts
+++ src/app/wais/memoria/memoria.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Resultado } from 'src/app/calculo/interfaces/resultado.interface';
+import { GlobalService } from 'src/app/comun/global.service';
+import { ResultadosService } from 'src/app/comun/resultados.service';
 
 @Component({
   selector: 'app-memoria',
@@ -13,18 +16,22 @@ export class MemoriaComponent implements OnInit{
   reloj:string = "00:00";
   timer:any;
   timememorytest:number = 20;
-  array:number[] = [];
+  array:number[] = [0,0,0,0,0,0,0,0,0];
   previo:boolean = true;
+  generado:number =0;
+  pos:number =0;
+  puntuacion:number =0;
+  terminado:boolean= false;
 
-
-  constructor(private formBuilder:FormBuilder) {}
+  constructor(private formBuilder:FormBuilder,
+    private globalService:GlobalService,
+    private resultadosService:ResultadosService) {}
 
   ngOnInit(): void {
 
     this.memoriaForm = this.formBuilder.group(
       {
-      generado:[""],
-      generado10:[""],
+      generado0:[""],
       generado1:[""],
       generado2:[""],
       generado3:[""],
@@ -32,8 +39,7 @@ export class MemoriaComponent implements OnInit{
       generado5:[""],
       generado6:[""],
       generado7:[""],
-      generado8:[""],
-      generado9:[""]
+      generado8:[""]
       });
     
   }
@@ -47,7 +53,15 @@ export class MemoriaComponent implements OnInit{
 
   finalizar(){
     clearInterval(this.timer);
-    alert(this.array)
+   
+    for (let i:number=0; i<=8 ; i++){
+      if ( this.memoriaForm.controls["generado"+i].value == ""+this.array[i]){
+        this.puntuacion++;
+      }
+    }
+    this.terminado = true;
+
+
   }
 
   contador(){
@@ -55,18 +69,19 @@ export class MemoriaComponent implements OnInit{
     if ( this.timememorytest > 0){
       this.timememorytest--;
       
+      // cada dos segundos cambia de número y posición
       if ( this.timememorytest % 2 ){
-        let num:number = Math.round(Math.random() * 9999);
-        this.array.push(num);
-        this.memoriaForm.controls["generado"].setValue(""+num);
+       this.generado = Math.round(Math.random() * 9999);
+        this.array[this.pos] = this.generado;
+        this.pos++;
       }
     }else{
       this.previo = false;
     }
 
-    //termina la generacion de números y empieza el tiempo para escribirlos en orden
+    //terminada la generacion de números, empieza el tiempo para escribirlos en orden
     if ( !this.previo){
-      this.memoriaForm.controls["generado"].setValue("");
+      this.generado = 0;
       this.tiempo = this.tiempo +1;
       let min:string = ""+ Math.trunc(this.tiempo / 60);
       min.length == 1 ? min = "0" + min : min = min;
@@ -76,4 +91,23 @@ export class MemoriaComponent implements OnInit{
     }
 
   }
+
+  guardar(){
+
+    let res:Resultado = {
+      id_resultado:null,
+      id_usuario:  this.globalService.usuario.id_usuario,
+      categoria:"wais4",
+      tipo:"memoria",
+      tiempo_total: this.tiempo,
+      puntuacion: this.puntuacion,
+      fecha:new Date()
+      } 
+    
+    this.resultadosService.setResultado(res).subscribe(() => {
+      alert("Registro guardado");
+    });
+  }
+
+
 }
--- src/app/wais/razonamiento/razonamiento.component.ts
+++ src/app/wais/razonamiento/razonamiento.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Resultado } from 'src/app/calculo/interfaces/resultado.interface';
+import { GlobalService } from 'src/app/comun/global.service';
+import { ResultadosService } from 'src/app/comun/resultados.service';
 
 @Component({
   selector: 'app-razonamiento',
@@ -6,5 +9,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./razonamiento.component.css']
 })
 export class RazonamientoComponent {
+
+  tiempo:number = 0;
+  reloj:string = "00:00";
+  timer:any;
+  cubos:string[] = ["./assets/c2.png","./assets/c2.png","./assets/c2.png","./assets/c2.png","./assets/c2.png","./assets/c2.png","./assets/c2.png","./assets/c2.png","./assets/c2.png"];
+
+  constructor(  private globalService:GlobalService,
+                private resultadosService:ResultadosService){}
+
+  iniciar(){
+    this.generar();
+    this.timer = setInterval( () => {
+      this.contador();
+    }, 1000 );
+  }
+
+  finalizar(){
+      clearInterval(this.timer);    
+  }
+
+
+  generar(){
+
+    for (var i=0; i<9; i++){
+      let generado = Math.round(Math.random() * 3);
+      if (generado == 1){
+          this.cubos[i] = "./assets/c1.png";
+      }else if(generado == 2){
+        this.cubos[i] = "./assets/c2.png";
+      }else{
+
+        let giro = Math.round(Math.random() * 4);
+        if ( giro == 0){
+          this.cubos[i] = "./assets/c3a.png";
+        }else if ( giro == 1){
+          this.cubos[i] = "./assets/c3b.png";
+        }else if ( giro == 2){
+          this.cubos[i] = "./assets/c3c.png";
+        }else{
+          this.cubos[i] = "./assets/c3d.png";
+        }
+      }
+
+    }
+
+
+  }
+
+
+  contador(){
+
+    this.tiempo = this.tiempo +1;
+    let min:string = ""+ Math.trunc(this.tiempo / 60);
+    min.length == 1 ? min = "0" + min : min = min;
+    let seg:string = ""+ Math.trunc(this.tiempo % 60);
+    seg.length == 1 ? seg = "0" + seg : seg = seg;
+    this.reloj = min + ":" + seg;
+  }
+
+  guardar(){
+
+    let res:Resultado = {
+      id_resultado:null,
+      id_usuario:  this.globalService.usuario.id_usuario,
+      categoria:"wais4",
+      tipo:"cubos",
+      tiempo_total: this.tiempo,
+      puntuacion: 1,
+      fecha:new Date()
+      } 
+    
+    this.resultadosService.setResultado(res).subscribe(() => {
+      alert("Registro guardado");
+    });
+  }
 
 }
